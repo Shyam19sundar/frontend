c5dfc87d1c16e47240877a7157bb5248aacd1117
Shyam19sundar <shyamsundar1952001@gmail.com>
2021-03-14 18:27:34 +0530

    final

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "https://desolate-fortress-07828.herokuapp.com/"
+    baseURL: "https://desolate-fortress-07828.herokuapp.com"
 })
 export default instance;
